968d1ed7e93b9fa1abbdb587a6907fc255967670
Jason Shin <visualbbasic@gmail.com>
2012-10-16 17:29:46 +1100

    after editing mcaptch

--- lesson164_index.php
+++ lesson164_index.php
@@ -1,7 +1,22 @@
 <?php
 session_start();
+if (isset($_POST['answer']) && !empty($_POST['answer'])) {
 
-$_SESSION['secure'] = rand(1000,9999);
+    $answer = $_POST['answer'];
+    $real_answer = $_SESSION['secure'];
+    echo '<p>Answer: ' . $answer . '<br />' . $real_answer . '</p>';
+    if ($answer == $real_answer) {
+        echo 'matched!';
+    }
+}
+
+$_SESSION['secure'] = rand(1000, 9999);
 ?>
 
-<img src="lesson164_create_captcha.php" />
+<p>
+    <img src="lesson164_create_captcha.php" />
+<form action="lesson164_index.php" method="POST">
+    <p><b>Type value:</b><input type="text" name="answer"></p>
+    <p><input type="submit" value="Check!"></p>
+</form>
+</p>
